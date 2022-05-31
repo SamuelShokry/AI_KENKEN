eb553707003548914eef1a089923cdae0cee315c
AntwanEmil <42111689+AntwanEmil@users.noreply.github.com>
2022-05-31 07:50:39 +0200

    arc cells fns

--- source_code/back/arc.cpp
+++ source_code/back/arc.cpp
@@ -1,3 +1,6 @@
+#include "arc.h"
+
+
 bool Arc::remove_inconsistant_values(std::vector <int>* firstDomain, std::vector <int>* secondDomain)
 {
     bool result = false;
@@ -148,3 +151,52 @@ bool Arc::remove_inconsistant_values(std::vector <int>* firstDomain, std::vector
 
     return result;
 }
+
+void Arc::set_arc(int cell1, int cell2, char c, int value)
+{
+    firstCell = cell1;
+    secondCell = cell2;
+    constraintChar = c;
+    constraintValue = value;
+}
+
+int Arc::get_first_cell(void)
+{
+    return firstCell;
+}
+
+int Arc::get_second_cell(void)
+{
+    return secondCell;
+}
+
+char Arc::get_constraint_char(void)
+{
+    return constraintChar;
+}
+
+int Arc::get_constraint_value(void)
+{
+    return constraintValue;
+}
+
+bool Arc::is_equal(Arc &x)
+{
+    return (x.get_first_cell() == firstCell
+            && x.get_second_cell() == secondCell
+            && x.get_constraint_char() == constraintChar);
+}
+
+void Arc::swap_cells(void)
+{
+    int temp = firstCell;
+    firstCell = secondCell;
+    secondCell = temp;
+}
+
+void Arc::print_arc(void)
+{
+	std::cout << firstCell <<' ' << secondCell << ' ' << constraintChar << ' ' << constraintValue << std::endl;
+}
+
+
