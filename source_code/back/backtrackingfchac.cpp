f2b2cd6e0e74de8a428f867ca644b508a01b21f6
AntwanEmil <42111689+AntwanEmil@users.noreply.github.com>
2022-05-31 07:44:45 +0200

    AC3 Backtracking

--- source_code/back/backtrackingfchac.cpp
+++ source_code/back/backtrackingfchac.cpp
@@ -106,3 +106,74 @@ BacktrackingFCHAC::BacktrackingFCHAC(grid* gridPtr)
         arcsFlag.push_back(arcsF);
 	}
 }
+
+
+
+bool BacktrackingFCHAC::AC_3()
+{
+	while(!(this->q.empty()))
+	{
+		Arc currentArc = this->q.front();
+		this->q.pop();
+		int celli = currentArc.get_first_cell();
+		int cellj = currentArc.get_second_cell();
+
+		//Loop through all the arcs of the celli to find the index of the arc with cellj to put it false.
+        auto begin = arcsPerCell[celli].begin();
+        auto end = arcsPerCell[celli].end();
+        for (auto it = begin; it != end; ++it)
+		{
+            if(currentArc.is_equal(*it))
+			{
+                arcsFlag[celli][it - begin] = false; //Delete the arc.
+			}
+		}
+
+		if(currentArc.remove_inconsistant_values(&Domains[celli], &Domains[cellj]))
+		{
+			if(Domains[celli].empty())
+				return false;
+            //int arcsSize = arcsPerCell[celli].size();  //Number of arcs to the celli.
+			//Loop through all cell1 neighbors (get all cell1 arcs from arcsPerCell vector).
+            for(auto iter = begin; iter != end; ++iter)
+			{
+                //Arc tempArc = arcsPerCell[celli][i];     //get the first arr of celli arcs.
+                int cellk = iter->get_second_cell();   //get the neighbor cell in this arc.
+				if(cellk != cellj)                       //check if the current arc isn't the removed arc in the first algorithm.
+				{
+					//swap the arc
+                    iter->swap_cells();
+                    //int arcsKsize = arcsPerCell[cellk].size();  //Number of arcs to the cellk.
+					//Loop through all the arcs of the cellk to find the index of the arc with celli
+                    auto beginK = arcsPerCell[cellk].begin();
+                    auto endK = arcsPerCell[cellk].end();
+                    for(auto iterK = beginK; iterK != endK; ++iterK)
+					{
+                        if(iter->is_equal(*iterK))
+						{
+                            if(arcsFlag[cellk][iterK - beginK] == false)
+							{
+                                q.push(*iter);
+							}
+							break;
+						}
+					}
+                    iter->swap_cells();
+				}
+			}
+		}
+	}
+	return true;
+}
+
+void BacktrackingFCHAC::solve(grid* gridPtr)
+{
+	if(this->AC_3())
+	{
+		BacktrackingFCH x(gridPtr);
+		x.initialize_domain(&Domains);
+		x.solve(0);
+		x.delete_BacktrackingFCH();
+	}
+}
+
