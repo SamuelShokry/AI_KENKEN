960c6954d23ac4a78350eeb53c084da6b6fe12e8
Salma sherif <40717302+Salmasherif33@users.noreply.github.com>
2022-06-02 22:20:43 +0200

    Add solver.cpp

---
+++ source_code/back/solver.cpp
@@ -0,0 +1,72 @@
+#include "solver.h"
+#include "backtracking.h"
+#include "backtrackingfch.h"
+#include "backtrackingfchac.h"
+
+void solver::set_game_solving_technique(solving_technique technique)
+{
+    gameSolvingTechnique = technique;
+}
+
+solving_technique solver::get_game_solving_technique()
+{
+    return gameSolvingTechnique;
+}
+
+void* solver::get_solver_ptr()
+{
+    return solverPtr;
+}
+
+void solver::solve(grid* gridPtr)
+{
+    switch(gameSolvingTechnique)
+    {
+    case BACKTRACKING:
+        solverPtr = new Backtracking(gridPtr);
+        static_cast<Backtracking*>(solverPtr)->solve(0);
+        break;
+
+    case BACKTRACKING_WITH_FORWARD_CHECKING:
+        solverPtr = new BacktrackingFCH(gridPtr);
+        static_cast<BacktrackingFCH*>(solverPtr)->initialize_domain();
+        static_cast<BacktrackingFCH*>(solverPtr)->solve(0);
+        break;
+
+    case BACKTRACKING_WITH_FORWARDCHECKING_AND_ARC_CONSISTENCY:
+        solverPtr = new BacktrackingFCHAC(gridPtr);
+        static_cast<BacktrackingFCHAC*>(solverPtr)->solve(gridPtr);
+        break;
+    }
+}
+
+void solver::clear_solution(grid* gridPtr)
+{
+    int gridSize = gridPtr->get_grid_size();
+    for(int i=0; i<(gridSize*gridSize); i++)
+    {
+        gridPtr->get_cells_ptr()[i].set_cell_value(0);
+    }
+}
+
+void solver::delete_solver()
+{
+    if (solverPtr == nullptr)
+        return;
+    switch(gameSolvingTechnique)
+    {
+    case BACKTRACKING:
+        delete((Backtracking*) solverPtr);
+        break;
+
+    case BACKTRACKING_WITH_FORWARD_CHECKING:
+        static_cast<BacktrackingFCH*>(solverPtr)->delete_BacktrackingFCH();
+        delete((BacktrackingFCH*) solverPtr);
+        break;
+
+    case BACKTRACKING_WITH_FORWARDCHECKING_AND_ARC_CONSISTENCY:
+        delete((BacktrackingFCHAC*) solverPtr);
+        break;
+    }
+    solverPtr = nullptr;
+}
